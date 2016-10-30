1fa6fb1bbbda26162da6f2b05a788607166319f6
Kim Frost <kim@thatworks.dk>
2016-10-30 14:48:22 +0100

    ()

--- Source/SpaceKatana_POC/ShipModule.cpp
+++ Source/SpaceKatana_POC/ShipModule.cpp
@@ -10,6 +10,8 @@ AShipModule::AShipModule()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	MaxHealth = 1000.f;
+	Health = Health;
 }
 
 // Called when the game starts or when spawned
