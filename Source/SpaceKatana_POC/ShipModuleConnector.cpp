22f5c18fbfd602866f52e66190f4aec1d8397e3a
Kim Frost <kim@thatworks.dk>
2016-12-12 20:52:10 +0100

    ()

--- Source/SpaceKatana_POC/ShipModuleConnector.cpp
+++ Source/SpaceKatana_POC/ShipModuleConnector.cpp
@@ -11,6 +11,8 @@ AShipModuleConnector::AShipModuleConnector()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bIsHazard = false;
+	bIsConnectable = false;
 }
 
 
--- Source/SpaceKatana_POC/ShipModuleConnector.h
+++ Source/SpaceKatana_POC/ShipModuleConnector.h
@@ -14,17 +14,24 @@ public:
 	// Sets default values for this actor's properties
 	AShipModuleConnector();
 
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Connector")
+	AShipModuleConnector* BoundTo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Connector")
+	bool bIsHazard;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Connector")
+	bool bIsConnectable;
+
+	UFUNCTION(BlueprintCallable, Category = "Data")
+	void Join(AShipModuleConnector* ConnectTo, bool bUpdateTransform, bool bUpdateOther);
+
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	
 	// Called every frame
 	virtual void Tick( float DeltaSeconds ) override;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Connector")
-	AShipModuleConnector* BoundTo;
-	
-	UFUNCTION(BlueprintCallable, Category = "Data")
-	void Join(AShipModuleConnector* ConnectTo, bool bUpdateTransform, bool bUpdateOther);
-
 
 };
